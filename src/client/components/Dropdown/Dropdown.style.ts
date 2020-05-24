bfc3a21aa91cdcc1aa00de4858c16347dad7190e
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-05-24 11:17:03 +0530

    created custom drop down

--- src/client/components/Dropdown/Dropdown.style.ts
+++ src/client/components/Dropdown/Dropdown.style.ts
@@ -1,23 +1,49 @@
-import  { css } from 'styled-components';
+import { css } from 'styled-components';
 
 export default css`
-  &.listbox-area{
-    
-    .dropdown-button{
+  &.listbox-area {
+    position: relative;
+    height: 100px;
+    border: 2px solid grey;
+    .listbox-dropdown-container {
+      width: 100%;
+      position: relative;
+      height: 48px;
+      top: 52px;
+    }
+
+    .dropdown-button {
+      width: 100%;
       padding: 10px 50px;
       background-color: white;
       border: 1px solid grey;
       cursor: pointer;
       border-radius: 5px;
+      height: 100%;
     }
 
-    .hidden{
+    .hidden {
       display: none;
     }
 
-    ul{
+    ul {
+      width: 100%;
       list-style: none;
       padding: 0;
+      margin: 0;
+      position: absolute;
+      top: 100%;
+
+      li {
+        width: 100%;
+        text-align: center;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border: 1px solid grey;
+        cursor: pointer;
+        background-color: ${(props) =>
+          props.theme.COLORS ? props.theme.COLORS.BACKGROUND.PRIMARY : 'white'};
+      }
     }
   }
-`
+`;
--- src/client/components/Dropdown/Dropdown.tsx
+++ src/client/components/Dropdown/Dropdown.tsx
@@ -3,48 +3,88 @@ import withStyles from '../../utils/withStyles';
 import styles from './Dropdown.style';
 
 type optionType = {
-  key : string;
-  value: React.ReactNode
+  key: string;
+  value: React.ReactNode;
 };
 
-const Dropdown = (props: { className: string; listBoxLabel: string; options: optionType[]; }) => {
-  
+const Dropdown = (props: {
+  className: string;
+  listBoxLabel: string;
+  options: optionType[];
+  onChange?: (e: any) => void;
+}) => {
   const cssClasses = `listbox-area ${props.className}`;
-  
-  const [ state, setState ] = useState({
-    selected: 'DARK',
-    listOpen: false
+
+  const [state, setState] = useState({
+    selected: props.options[0],
+    listOpen: false,
   });
 
-  const getOptions = (options: optionType[]) => {
-    return options.map( (option: optionType) => <li id={option.key} key={option.key} data-key={option.key} role="option" onClick={handleOptionClick}>{option.value}</li>)
-  }
+  const getOptions = (options: optionType[]) =>
+    options.map((option: optionType) => (
+      <li
+        id={option.key}
+        key={option.key}
+        data-key={option.key}
+        role='option'
+        onClick={handleOptionClick}
+      >
+        {option.value}
+      </li>
+    ));
 
   const handleButtonClick = (e: any) => {
     console.log(e);
     setState({
       ...state,
-      listOpen: !state.listOpen
-    })
-  }
+      listOpen: !state.listOpen,
+    });
+  };
 
-  const handleOptionClick = (e: { target: any; } ) => {
-    console.log('event target ',  e.target);
-  }
-  
+  const handleOptionClick = (e: any) => {
+    const {
+      target: {
+        dataset: { key },
+      },
+    } = e;
+    const selected = props.options.filter((option) => option.key === key);
+    setState({
+      listOpen: false,
+      selected: selected[0],
+    });
+
+    props.onChange && props.onChange(e);
+  };
+
+  const { options } = props;
+
   return (
     <div className={cssClasses}>
-      <span className="listbox-label" id="listbox-label">{props.listBoxLabel}</span>
-      <div className="listbox-dropdown-container">
-        <button onClick={handleButtonClick} className="dropdown-button" id="dropdown-button" aria-labelledby="listbox-label dropdown-button" aria-haspopup="listbox">
-          {state.selected}
+      <span className='listbox-label' id='listbox-label'>
+        {props.listBoxLabel}
+      </span>
+      <div className='listbox-dropdown-container'>
+        <button
+          onClick={handleButtonClick}
+          className='dropdown-button'
+          id='dropdown-button'
+          aria-labelledby='listbox-label dropdown-button'
+          aria-haspopup='listbox'
+        >
+          {state.selected.value}
         </button>
-        <ul className={!state.listOpen ? 'hidden' : ''} id="listbox-dropdown-list" role="listbox" aria-labelledby="listbox-label" aria-activedescendent="VINE">
-          {getOptions(props.options)}
+        <ul
+          className={!state.listOpen ? 'hidden' : ''}
+          id='listbox-dropdown-list'
+          role='listbox'
+          aria-labelledby='listbox-label'
+          aria-activedescendent='VINE'
+        >
+          {options && getOptions(options)}
         </ul>
       </div>
     </div>
   );
-}
+};
 
-export default withStyles(styles,Dropdown);
+export default withStyles(styles, Dropdown);
