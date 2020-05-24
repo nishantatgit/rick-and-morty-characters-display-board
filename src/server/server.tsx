d45bfdff5bf7d4f22ff5e5389665689f2b8bcc49
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-05-24 11:27:32 +0530

    server changes

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -7,7 +7,7 @@ import Pagecontainer from '../client/Container/PageContainer/PageContainer';
 const app = express();
 
 const sheet = new ServerStyleSheet();
-const content = renderToString(sheet.collectStyles(<Pagecontainer/>));
+const content = renderToString(sheet.collectStyles(<Pagecontainer />));
 const styleTags = sheet.getStyleTags();
 sheet.seal();
 const html = Html(content);
@@ -18,14 +18,12 @@ console.log('sheet ', sheet);
 console.log('styleTags', styleTags);
 console.log('-----------------------');
 
-
 app.use(express.static('./public'));
 
-app.get('/', (req: any,res: { send: (content: string) => void; }) => {
+app.get('/', (req: any, res: { send: (content: string) => void }) => {
   res.send(html);
 });
 
-app.listen('7001', (req, res) => {
+app.listen('7001', (req: any, res: any) => {
   console.log('server listening on port 7001');
 });
-
