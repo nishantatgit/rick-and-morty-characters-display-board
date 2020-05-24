6fcf4becf5199fa0236135ddf5b0d78f1b079ed8
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-05-24 11:16:20 +0530

    added font style

---
+++ src/client/ui/themes/common/fonts.ts
@@ -0,0 +1,35 @@
+const FONT_WEIGHT = {
+  LIGHT: '300',
+  NORMAL: '400',
+  MEDIUM: '500',
+  SEMIBOLD: '600',
+  BOLD: '700',
+};
+
+const FONT_SIZE = {
+  HEADING: {
+    H1: 38,
+    H2: 32,
+    H3: 30,
+    H4: 24,
+    H5: 14,
+    H6: 12,
+  },
+  BODY: {
+    PRIMARY: 20,
+    SECONDARY: 16,
+    TERTIARY: 12,
+  },
+};
+
+const FONT_FAMILY = {
+  CURSIVE: 'Balsamiq Sans, cursive',
+  HEADING: 'Montserrat, sans-serif',
+  BODY: 'Raleway, sans-serif',
+};
+
+export default {
+  FONT_WEIGHT,
+  FONT_SIZE,
+  FONT_FAMILY,
+};
