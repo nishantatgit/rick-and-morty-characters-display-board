4cee29308852e391749b4d98a40d7e5b45035f1e
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-05-25 11:18:51 +0530

    added urls

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export default {
+  URLS: {
+    characters: 'https://rickandmortyapi.com/api/character',
+    locations: 'https://rickandmortyapi.com/api/location',
+    episodes: 'https://rickandmortyapi.com/api/episode',
+  },
+};
