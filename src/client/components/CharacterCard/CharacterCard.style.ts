0a242409d48378693fc5c292d332d5cb1540560a
nku107 <nishant.kumar.shreevastav@gmail.com>
2020-05-25 21:21:57 +0530

    added style for character card

--- src/client/components/CharacterCard/CharacterCard.style.ts
+++ src/client/components/CharacterCard/CharacterCard.style.ts
@@ -1,6 +1,26 @@
 import { css } from 'styled-components';
 
 export default css`
-  .character-card {
+  background-color: ${(props) => props.theme.COLORS.BACKGROUND.PRIMARY};
+  color: ${(props) => props.theme.COLORS.TEXT.PRIMARY};
+  display: flex;
+  flex-direction: column;
+  .character-card__details {
+    padding: 5px;
+  }
+  .character-card__details__name,
+  .character-card__details__status,
+  .character-card__details__gender,
+  .character-card__details__origin,
+  .character-card__details__location {
+    display: block;
+  }
+  .image-container {
+    width: 100%;
+    height: auto;
+
+    img {
+      max-width: 100%;
+    }
   }
 `;
